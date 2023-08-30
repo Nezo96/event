25568543b167b79dfbe7d47e9b99a9bf365aba79
Rado | Nezo96 <racradoslav96@gmail.com>
2023-08-30 15:13:04 +0200

    Added controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\GalerryController;
+use App\Http\Controllers\GalleryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +29,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/events', EventController::class);
+    Route::resource('/galleries', GalleryController::class);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
