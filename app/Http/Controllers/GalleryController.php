8c7b713aa30c62f668a53c749fe8cc162d590e56
Rado | Nezo96 <racradoslav96@gmail.com>
2023-08-30 15:17:15 +0200

    Created views for events and galleries

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class GalleryController extends Controller
@@ -9,9 +10,9 @@ class GalleryController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
-        //
+        return view('galleries.index');
     }
 
     /**
